c32d53e940101295097dc34b1d311f7f82e9af0c
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 12:51:18 +0200

    Update index.2.js

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+// Get the total number of drum buttons on the webpage
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+// Add click event listeners to all drum buttons
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    // Get the inner HTML of the clicked button
     var buttonInnerHTML = this.innerHTML;
 
+    // Call the makeSound function with the button's inner HTML
     makeSound(buttonInnerHTML);
 
+    // Call the buttonAnimation function with the button's inner HTML
     buttonAnimation(buttonInnerHTML);
   });
 }
 
+// Add keydown event listener to the whole document
 document.addEventListener("keydown", function (event) {
+  // Call the makeSound function with the pressed key
   makeSound(event.key);
 
+  // Call the buttonAnimation function with the pressed key
   buttonAnimation(event.key);
 });
 
+// Function to play the corresponding sound based on the key pressed
 function makeSound(key) {
   switch (key) {
     case "w":
@@ -47,18 +56,19 @@ function makeSound(key) {
       kick.play();
       break;
     default:
-      console.log(buttonInnerHTML);
+      console.log("Invalid key: " + key);
   }
 }
 
+// Function to add and remove the "pressed" class for button animation
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("." + currentKey);
 
+  // Add the "pressed" class to the active button
   activeButton.classList.add("pressed");
 
+  // Remove the "pressed" class after 100 milliseconds
   setTimeout(function () {
     activeButton.classList.remove("pressed");
   }, 100);
 }
-
-     
